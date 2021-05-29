a7659686f1d4cd763d1f53a9f89b8bf840852881
sngina <sngina707@gmail.com>
2021-05-29 13:42:13 +0300

    creat an object

--- user_test.py
+++ user_test.py
@@ -6,4 +6,8 @@ class  TestLogin(unittest.TestCase):
     def setup(self):
         self.login = Login("Stella","Ngina","0726091999","sngina707@gmail.com,munie")
     def test_init(self):
-        
+        self.assertEqual(self.login.first_name,"Stella") 
+        self.assertEqual(self.login.last_name,"Ngina")
+        self.assertEqual(self.login.phone_number,"0726091999")
+        self.assertEqual(self.login.email,"sngina707@gmail.com")
+        self.assertEqual(self.login.password,"munie")
