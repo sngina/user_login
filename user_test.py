adc0305ecd82160bbc4bd44b450bbc8c28e6f1c3
sngina <sngina707@gmail.com>
2021-05-30 11:02:06 +0300

    add display_credential function

--- user_test.py
+++ user_test.py
@@ -1,9 +1,10 @@
 
+from run import save_user
 import unittest
 from   user import  Login
 
 class  TestLogin(unittest.TestCase):
-    def setUp(self):
+    def setUp(self): #first setup
         self.login = Login("Stella","Ngina","0726091999","sngina707@gmail.com","munie")
     def test_init(self):
         self.assertEqual(self.login.first_name,"Stella") 
@@ -11,6 +12,20 @@ class  TestLogin(unittest.TestCase):
         self.assertEqual(self.login.phone_number,"0726091999")
         self.assertEqual(self.login.email,"sngina707@gmail.com")
         self.assertEqual(self.login.password,"munie")
+     
+    
+    def test_save_multiple_user(self): #test multiple save
+        self.create_credentials.save_user()
+        test_user = Login("Test","user","0721345678","test@user") # new contact
+        test_user.save_user()
+        self.assertEqual(len(Login.login_list),2)
+    def tearDown(self):
+        Login.login_list = []
+    def test_save_multiple_user(self):
+        self.create_credentials.save_user()
+        test_user = Login ("Test","user","0712345678","test@user.com") #new contact
+        test_user.save_user()
+        self.assertEqual(len(Login.login_list),2)
 
 
 if __name__ == '__main__':
